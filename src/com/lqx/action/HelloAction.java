4bcf3029c6cda7d00392c653922dbf6ab7a00092
530 <530882363@qq.com>
2016-08-29 21:19:46 +0800

    项目提交

---
+++ src/com/lqx/action/HelloAction.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.lqx.action;
+
+/**
+ * @author Jack
+ *
+ */
+public class HelloAction {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("helloWorld");
+	}
+
+}
