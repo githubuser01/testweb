c8a90a792ee3ff3f93c27541e9746b614857fbd0
530 <530882363@qq.com>
2016-08-29 21:31:07 +0800

    add jack

---
+++ src/com/lqx/action/JackAction.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.lqx.action;
+
+/**
+ * @author Jack
+ *
+ */
+public class JackAction {
+	
+	int age;
+	
+	String name;
+	
+	String sex;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+
+}
