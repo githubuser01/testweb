d787f1852409b46ce999dfd8a68a1232fdb61f4f
530 <530882363@qq.com>
2016-08-29 21:48:10 +0800

    comment

---
+++ src/com/lqx/action/Action.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.lqx.action;
+
+/**
+ * @author Jack
+ *
+ */
+public class Action {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("helloworld");
+	}
+
+}
